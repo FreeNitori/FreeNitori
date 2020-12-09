515ce7f6df2a72accd86f5a60a5291efa558ea0a
RandomChars <random@chars.jp>
2020-12-09 19:37:29 +0900

    fix text area, proper form display todo

--- assets/web/static/js/lookup.js
+++ assets/web/static/js/lookup.js
@@ -1,11 +1,12 @@
-document.getElementById("snowflake").addEventListener("submit", function (event) {
-    lookup(document.getElementById('snowflake').value);
-    event.preventDefault();
+document.getElementById("snowflake").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        lookup(document.getElementById('snowflake').value);
+    }
 }, false);
 
 function lookup(snowflake) {
     let userInfo = fetchJSON("http://localhost:7777/api/user/" + snowflake)
     userInfo.then(function (data) {
-        document.getElementById("result").innerHTML = data.text;
+        document.getElementById("result").innerHTML = data["Name"];
     })
 }
