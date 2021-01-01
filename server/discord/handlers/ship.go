7552f793e4d6d3f6005dde5c253129fb99ba267b
RandomChars <random@chars.jp>
2021-01-01 22:25:22 +0900

    use % operator

--- server/discord/handlers/ship.go
+++ server/discord/handlers/ship.go
@@ -5,7 +5,6 @@ import (
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/embedutil"
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/multiplexer"
 	"git.randomchars.net/RandomChars/FreeNitori/server/discord/vars"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -35,7 +34,7 @@ func ship(context *multiplexer.Context) {
 		context.SendMessage(vars.MissingUser)
 		return
 	}
-	res := int(math.Mod(float64(func() int { id, _ := strconv.Atoi(member1.User.ID); return id }()^func() int { id, _ := strconv.Atoi(member2.User.ID); return id }()), 101))
+	res := (func() int { id, _ := strconv.Atoi(member1.User.ID); return id }() ^ func() int { id, _ := strconv.Atoi(member2.User.ID); return id }()) % 101
 	embed := embedutil.NewEmbed(
 		fmt.Sprintf("`%s` ❤️ `%s`", member1.User.Username, member2.User.Username),
 		fmt.Sprintf("[%s%s] %v", strings.Repeat("=", res/2), strings.Repeat("-", 50-res/2), res)+"%")
