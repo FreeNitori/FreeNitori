46783fc54ba0dd442f5ad3dafacc76e85e1b3469
RandomChars <random@chars.jp>
2021-01-02 17:08:35 +0900

    fix example

--- plugins/feels/main.go
+++ plugins/feels/main.go
@@ -10,7 +10,7 @@ var CommandRoute = multiplexer.Route{
 	Pattern:       "feels",
 	AliasPatterns: []string{"feelsgreat"},
 	Description:   "Sends a feels great emote.",
-	Category:      *multiplexer.SystemCategory,
+	Category:      multiplexer.SystemCategory,
 	Handler: func(context *multiplexer.Context) {
 		context.SendMessage("<:FeelsKappa:713635502741520384>")
 	},
