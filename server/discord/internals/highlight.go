6e47c26b80341d41c55f31f6297bc6eb16c7fdf9
RandomChars <random@chars.jp>
2021-01-15 13:16:17 +0900

    move handling to one function

--- server/discord/internals/highlight.go
+++ server/discord/internals/highlight.go
@@ -3,10 +3,13 @@ package internals
 import (
 	"fmt"
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/config"
+	"git.randomchars.net/RandomChars/FreeNitori/nitori/embedutil"
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/emoji"
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/multiplexer"
 	"git.randomchars.net/RandomChars/FreeNitori/nitori/overrides"
+	"git.randomchars.net/RandomChars/FreeNitori/server/discord/vars"
 	"github.com/bwmarrin/discordgo"
+	"strconv"
 	"unicode/utf8"
 )
 
@@ -69,18 +72,53 @@ func init() {
 					return "Current emoji", value, true
 				},
 			},
+			{
+				Name:         "amount",
+				FriendlyName: "Minimum Requirement",
+				Description:  "Minimum amount of reactions required for highlighting.",
+				DatabaseKey:  "highlight_amount",
+				Cleanup:      func(context *multiplexer.Context) { config.ResetGuildMap(context.Guild.ID, "highlight") },
+				Validate: func(context *multiplexer.Context, input *string) (bool, bool) {
+					amount, err := strconv.Atoi(*input)
+					if err != nil {
+						return false, true
+					}
+					if amount > 16 || amount < 1 {
+						return false, true
+					}
+					return true, true
+				},
+				Format: func(context *multiplexer.Context, value string) (string, string, bool) {
+					if value == "" {
+						return "Not configured", fmt.Sprintf(fmt.Sprintf("Configure by issuing command `%sconf highlight amount <integer>`.", context.Prefix())), true
+					}
+					return "Current requirement", value + " reactions", true
+				},
+			},
 		},
 	})
 }
 
-func addReaction(session *discordgo.Session, add *discordgo.MessageReactionAdd) {
-	channelID, err := config.GetGuildConfValue(add.GuildID, "highlight_channel")
+func handleHighlightReaction(session *discordgo.Session, reaction *discordgo.MessageReaction) {
+	channelID, err := config.GetGuildConfValue(reaction.GuildID, "highlight_channel")
 	if err != nil {
 		return
 	}
-	guild, err := session.State.Guild(add.GuildID)
+	amountString, err := config.GetGuildConfValue(reaction.GuildID, "highlight_amount")
 	if err != nil {
 		return
+	}
+	amount, err := strconv.Atoi(amountString)
+	if err != nil {
+		return
+	}
+	guild, err := session.State.Guild(reaction.GuildID)
+	if err != nil {
+		guild, err = session.Guild(reaction.GuildID)
+		if err != nil {
+			return
+		}
+		_ = session.State.GuildAdd(guild)
 	}
 	var channel *discordgo.Channel
 	for _, c := range guild.Channels {
@@ -92,47 +130,49 @@ func addReaction(session *discordgo.Session, add *discordgo.MessageReactionAdd) 
 	if channel == nil {
 		return
 	}
-	if add.Emoji.ID != "null" {
+	if reaction.Emoji.ID != "null" {
 		return
 	}
 	e, err := config.GetGuildConfValue(guild.ID, "highlight_emoji")
 	if err != nil {
 		return
 	}
-	if add.Emoji.Name != e {
+	if reaction.Emoji.Name != e {
 		return
 	}
-	// TODO: handler thing
+
+	message, err := session.State.Message(reaction.ChannelID, reaction.MessageID)
+	if err != nil {
+		message, err = session.ChannelMessage(reaction.ChannelID, reaction.MessageID)
+		if err != nil {
+			return
+		}
+		_ = session.State.MessageAdd(message)
+	}
+
+	for _, reactions := range message.Reactions {
+		if reactions.Emoji.Name == e {
+			if reactions.Count >= amount {
+				// TODO: database thing
+				embed := embedutil.NewEmbed("", message.Content)
+				embed.SetAuthor(message.Author.Username+message.Author.Discriminator, message.Author.Avatar)
+				embed.Color = vars.KappaColor
+				embed.AddField("Original Message", fmt.Sprintf("[Redirect](https://discord.com/channels/%s/%s/%s)", guild.ID, channel.ID, message.ID), false)
+				_, err := session.ChannelMessageSendEmbed(channel.ID, embed.MessageEmbed)
+				if err != nil {
+					return
+				}
+				// FIXME: premature return
+			}
+			break
+		}
+	}
+}
+
+func addReaction(session *discordgo.Session, add *discordgo.MessageReactionAdd) {
+	handleHighlightReaction(session, add.MessageReaction)
 }
 
 func removeReaction(session *discordgo.Session, remove *discordgo.MessageReactionRemove) {
-	channelID, err := config.GetGuildConfValue(remove.GuildID, "highlight_channel")
-	if err != nil {
-		return
-	}
-	guild, err := session.State.Guild(remove.GuildID)
-	if err != nil {
-		return
-	}
-	var channel *discordgo.Channel
-	for _, c := range guild.Channels {
-		if channelID == c.ID {
-			channel = c
-			break
-		}
-	}
-	if channel == nil {
-		return
-	}
-	if remove.Emoji.ID != "null" {
-		return
-	}
-	e, err := config.GetGuildConfValue(guild.ID, "highlight_emoji")
-	if err != nil {
-		return
-	}
-	if remove.Emoji.Name != e {
-		return
-	}
-	// TODO: handler thing
+	handleHighlightReaction(session, remove.MessageReaction)
 }
