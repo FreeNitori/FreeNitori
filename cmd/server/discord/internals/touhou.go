1b483ae2aafa1dd39eaf08e6bd0af434d16dd294
RandomChars <random@chars.jp>
2021-02-21 15:09:52 +0900

    separate image fetching stuff to its own package

--- cmd/server/discord/internals/touhou.go
+++ cmd/server/discord/internals/touhou.go
@@ -1,209 +1,17 @@
 package internals
 
 import (
-	"encoding/xml"
-	"errors"
 	"fmt"
 	"git.randomchars.net/FreeNitori/FreeNitori/nitori/embedutil"
 	"git.randomchars.net/FreeNitori/FreeNitori/nitori/multiplexer"
 	"git.randomchars.net/FreeNitori/FreeNitori/nitori/state"
-	"io/ioutil"
+	imagefetch "git.randomchars.net/FreeNitori/ImageFetch"
 	"math/rand"
-	"net/http"
 	"strings"
 	"time"
 )
 
-const ImageBoardQueryURL = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&" +
-	"tags=solo+-underwear+-sideboob+-pov_feet+-underboob+-upskirt+-" +
-	"sexually_suggestive+-ass+-bikini+-6%2Bgirls+-comic+-greyscale+-" +
-	"huge_filesize+-lovestruck+-absurdres+-artificial_vagina+-cookie_%28touhou%29+-" +
-	"covering_breasts+-huge_breasts+-blood+-penetration_gesture+-" +
-	"animated+-audio+-webm+rating:safe+"
-
-var ErrNoArtAvailable = errors.New("no art available")
 var sessions = map[string]chan [2]string{}
-
-// BoardPosts represents an array of posts returned by the image board.
-type BoardPosts struct {
-	XMLName xml.Name    `xml:"posts"`
-	Posts   []BoardPost `xml:"post"`
-	Count   int         `xml:"count,attr"`
-	Offset  int         `xml:"offset,attr"`
-}
-
-// BoardPost represents a post on the image board.
-type BoardPost struct {
-	Height        int    `xml:"height,attr"`
-	Title         string `xml:"title,attr"`
-	Score         int    `xml:"score,attr"`
-	FileURL       string `xml:"file_url,attr"`
-	ParentID      int    `xml:"parent_id,attr"`
-	SampleURL     string `xml:"sample_url,attr"`
-	SampleWidth   int    `xml:"sample_width,attr"`
-	SampleHeight  int    `xml:"sample_height,attr"`
-	PreviewURL    string `xml:"preview_url,attr"`
-	Rating        string `xml:"rating,attr"`
-	Tags          string `xml:"tags,attr"`
-	ID            int    `xml:"id,attr"`
-	Width         int    `xml:"width,attr"`
-	Change        int    `xml:"change,attr"`
-	MD5           string `xml:"md5,attr"`
-	CreatorID     int    `xml:"creator_id,attr"`
-	HasChildren   bool   `xml:"has_children,attr"`
-	CreatedAt     string `xml:"created_at,attr"`
-	Status        string `xml:"status,attr"`
-	Source        string `xml:"source,attr"`
-	HasNotes      bool   `xml:"has_notes,attr"`
-	HasComments   bool   `xml:"has_comments,attr"`
-	PreviewWidth  int    `xml:"preview_width,attr"`
-	PreviewHeight int    `xml:"preview_height,attr"`
-}
-
-// CharacterInfo represents information on a character.
-type CharacterInfo struct {
-	Color        int
-	FriendlyName string
-	SearchString string
-}
-
-// CharacterArt represents a piece of artwork of a character.
-type CharacterArt struct {
-	ImageURL  string
-	SourceURL string
-	Character CharacterInfo
-}
-
-// CharacterList returns a slice of CharacterInfo.
-func CharacterList() []CharacterInfo {
-	return []CharacterInfo{
-		{Color: 0xb50404, FriendlyName: "Reimu", SearchString: "Hakurei_Reimu"},
-		{Color: 0x42d4f4, FriendlyName: "Ran", SearchString: "Yakumo_Ran"},
-		{Color: 0xff8ade, FriendlyName: "Mystia", SearchString: "Mystia_Lorelei"},
-		{Color: 0x42d4f4, FriendlyName: "Miyoi", SearchString: "Okunoda_Miyoi"},
-		{Color: 0x42d4f4, FriendlyName: "Mamizou", SearchString: "Futatsuiwa_Mamizou"},
-		{Color: 0x42d4f4, FriendlyName: "Eternity", SearchString: "Eternity_Larva"},
-		{Color: 0x0, FriendlyName: "Nue", SearchString: "Houjuu_Nue"},
-		{Color: 0x42d4f4, FriendlyName: "Konngara", SearchString: "Konngara"},
-		{Color: 0x42d4f4, FriendlyName: "Rinnosuke", SearchString: "Morichika_Rinnosuke"},
-		{Color: 0x42d4f4, FriendlyName: "Yumemi", SearchString: "Okazaki_Yumemi"},
-		{Color: 0x42d4f4, FriendlyName: "Lily", SearchString: "Lily_White"},
-		{Color: 0xdf9041, FriendlyName: "Kutaka", SearchString: "Niwatari_Kutaka"},
-		{Color: 0x42d4f4, FriendlyName: "Medicine", SearchString: "Medicine_Melancholy"},
-		{Color: 0xf5e942, FriendlyName: "Marisa", SearchString: "Kirisame_Marisa"},
-		{Color: 0xd25859, FriendlyName: "Raiko", SearchString: "Horikawa_Raiko"},
-		{Color: 0x4e7764, FriendlyName: "Mai", SearchString: "Teireida_Mai"},
-		{Color: 0xa84384, FriendlyName: "Yorihime", SearchString: "Watatsuki_No_Yorihime"},
-		{Color: 0x42d4f4, FriendlyName: "Nemuno", SearchString: "Sakata_Nemuno"},
-		{Color: 0x42d4f4, FriendlyName: "Suguri", SearchString: "Suguri_(character)"},
-		{Color: 0xc7c7c7, FriendlyName: "Sakuya", SearchString: "Izayoi_Sakuya"},
-		{Color: 0x42d4f4, FriendlyName: "Kisume", SearchString: "Kisume"},
-		{Color: 0x42d4f4, FriendlyName: "Star", SearchString: "Star_Sapphire"},
-		{Color: 0x42d4f4, FriendlyName: "Mima", SearchString: "Mima"},
-		{Color: 0x9917, FriendlyName: "Okuu", SearchString: "Reiuji_Utsuho"},
-		{Color: 0xb2daef, FriendlyName: "Nitori", SearchString: "Kawashiro_Nitori"},
-		{Color: 0xaa6ad3, FriendlyName: "Sumireko", SearchString: "Usami_Sumireko"},
-		{Color: 0x42d4f4, FriendlyName: "Sukuna", SearchString: "Sukuna_Shinmyoumaru"},
-		{Color: 0xf5da42, FriendlyName: "Ex_Rumia", SearchString: "Ex-Rumia"},
-		{Color: 0x42d4f4, FriendlyName: "Kokoro", SearchString: "Hata_No_Kokoro"},
-		{Color: 0x42d4f4, FriendlyName: "Futo", SearchString: "Mononobe_No_Futo"},
-		{Color: 0x42d4f4, FriendlyName: "Mayumi", SearchString: "Joutouguu_Mayumi"},
-		{Color: 0x42d4f4, FriendlyName: "Renko", SearchString: "Usami_Renko"},
-		{Color: 0x42d4f4, FriendlyName: "Wriggle", SearchString: "Wriggle_Nightbug"},
-		{Color: 0x14a625, FriendlyName: "Kokuu", SearchString: "Kokuu_Haruto"},
-		{Color: 0x42d4f4, FriendlyName: "Tenshi", SearchString: "Hinanawi_Tenshi"},
-		{Color: 0x79eb50, FriendlyName: "Youmu", SearchString: "Konpaku_Youmu"},
-		{Color: 0x42d4f4, FriendlyName: "Letty", SearchString: "Letty_Whiterock"},
-		{Color: 0x42d4f4, FriendlyName: "Minoriko", SearchString: "Aki_Minoriko"},
-		{Color: 0xe2a81e, FriendlyName: "Ringo", SearchString: "Ringo_Touhou"},
-		{Color: 0x24b343, FriendlyName: "Sanae", SearchString: "Kochiya_Sanae"},
-		{Color: 0x940f0f, FriendlyName: "Hecatia", SearchString: "Hecatia_Lapislazuli"},
-		{Color: 0x42d4f4, FriendlyName: "Merlin", SearchString: "Merlin_Prismriver"},
-		{Color: 0x42d4f4, FriendlyName: "Luna", SearchString: "Luna_Child"},
-		{Color: 0x42d4f4, FriendlyName: "Chiyuri", SearchString: "Kitashirakawa_Chiyuri"},
-		{Color: 0xa700f5, FriendlyName: "Satori", SearchString: "Komeiji_Satori"},
-		{Color: 0xfd8cff, FriendlyName: "Remilia", SearchString: "Remilia_Scarlet"},
-		{Color: 0x42d4f4, FriendlyName: "Hina", SearchString: "Kagiyama_Hina"},
-		{Color: 0x42d4f4, FriendlyName: "Eirin", SearchString: "Yagokoro_Eirin"},
-		{Color: 0xe58a53, FriendlyName: "Aya", SearchString: "Shameimaru_Aya"},
-		{Color: 0x42d4f4, FriendlyName: "Kagerou", SearchString: "Imaizumi_Kagerou"},
-		{Color: 0x42d4f4, FriendlyName: "Lunasa", SearchString: "Lunasa_Prismriver"},
-		{Color: 0xe5ff, FriendlyName: "Cirno", SearchString: "Cirno"},
-		{Color: 0x42d4f4, FriendlyName: "Sariel", SearchString: "Sariel"},
-		{Color: 0xf50000, FriendlyName: "Mokou", SearchString: "Fujiwara_No_Mokou"},
-		{Color: 0x42d4f4, FriendlyName: "Suika", SearchString: "Ibuki_Suika"},
-		{Color: 0x42d4f4, FriendlyName: "Sekibanki", SearchString: "Sekibanki"},
-		{Color: 0x42d4f4, FriendlyName: "Urumi", SearchString: "Ushizaki_Urumi"},
-		{Color: 0xb50404, FriendlyName: "Flandre", SearchString: "Flandre_Scarlet"},
-		{Color: 0x42d4f4, FriendlyName: "Aunn", SearchString: "Komano_Aun"},
-		{Color: 0xd25859, FriendlyName: "Komachi", SearchString: "Onozuka_Komachi"},
-		{Color: 0xaeb4c6, FriendlyName: "Seija", SearchString: "Kijin_Seija"},
-		{Color: 0xb50480, FriendlyName: "Hatate", SearchString: "Himekaidou_Hatate"},
-		{Color: 0x42d4f4, FriendlyName: "Momiji", SearchString: "Inubashiri_Momiji"},
-		{Color: 0x42d4f4, FriendlyName: "Parsee", SearchString: "Mizuhashi_Parsee"},
-		{Color: 0xef61ff, FriendlyName: "Kaguya", SearchString: "Houraisan_Kaguya"},
-		{Color: 0x990000, FriendlyName: "Koakuma", SearchString: "Koakuma"},
-		{Color: 0xe262b0, FriendlyName: "Satono", SearchString: "Nishida_Satono"},
-		{Color: 0x583b80, FriendlyName: "Toyohime", SearchString: "Watatsuki_No_Toyohime"},
-		{Color: 0xf94aff, FriendlyName: "Reisen", SearchString: "Reisen_Udongein_Inaba"},
-		{Color: 0x42d4f4, FriendlyName: "Kyouko", SearchString: "Kasodani_Kyouko"},
-		{Color: 0x42d4f4, FriendlyName: "Yoshika", SearchString: "Miyako_Yoshika"},
-		{Color: 0x42d4f4, FriendlyName: "Seiga", SearchString: "Kaku_Seiga"},
-		{Color: 0x42d4f4, FriendlyName: "Miko", SearchString: "Toyosatomimi_No_Miko"},
-		{Color: 0x2291ba, FriendlyName: "Rei'sen", SearchString: "Reisen"},
-		{Color: 0x42d4f4, FriendlyName: "Kogasa", SearchString: "Tatara_Kogasa"},
-		{Color: 0x24b343, FriendlyName: "Yuuka", SearchString: "Kazami_Yuuka"},
-		{Color: 0x42d4f4, FriendlyName: "Saki", SearchString: "Kurokoma_Saki"},
-		{Color: 0x42d4f4, FriendlyName: "Shinki", SearchString: "Shinki"},
-		{Color: 0x574b8c, FriendlyName: "Keine", SearchString: "Kamishirasawa_Keine"},
-		{Color: 0x42d4f4, FriendlyName: "Yachie", SearchString: "Kicchou_Yachie"},
-		{Color: 0xb4449c, FriendlyName: "Sagume", SearchString: "Kishin_Sagume"},
-		{Color: 0x42d4f4, FriendlyName: "Yamame", SearchString: "Kurodani_Yamame"},
-		{Color: 0x42d4f4, FriendlyName: "Chen", SearchString: "Chen"},
-		{Color: 0x4b548, FriendlyName: "Meiling", SearchString: "Hong_Meiling"},
-		{Color: 0x42d4f4, FriendlyName: "Iku", SearchString: "Nagae_Iku"},
-		{Color: 0xc646e0, FriendlyName: "Patchouli", SearchString: "Patchouli_Knowledge"},
-		{Color: 0x42d4f4, FriendlyName: "Nazrin", SearchString: "Nazrin"},
-		{Color: 0xcc7c9c, FriendlyName: "Tewi", SearchString: "Inaba_Tewi"},
-		{Color: 0x42d4f4, FriendlyName: "Eika", SearchString: "Ebisu_Eika"},
-		{Color: 0x42d4f4, FriendlyName: "Shou", SearchString: "Toramaru_Shou"},
-		{Color: 0x42d4f4, FriendlyName: "Wakasagihime", SearchString: "Wakasagihime"},
-		{Color: 0x42d4f4, FriendlyName: "Alice", SearchString: "Alice_Margatroid"},
-		{Color: 0x42d4f4, FriendlyName: "Genjii", SearchString: "Genjii"},
-		{Color: 0xaa4fa0, FriendlyName: "Joon", SearchString: "Yorigami_Joon"},
-		{Color: 0x977cac, FriendlyName: "Kanako", SearchString: "Yasaka_Kanako"},
-		{Color: 0x42d4f4, FriendlyName: "Lyrica", SearchString: "Lyrica_Prismriver"},
-		{Color: 0x42d4f4, FriendlyName: "Keiki", SearchString: "Haniyasushin_Keiki"},
-		{Color: 0x42d4f4, FriendlyName: "Orin", SearchString: "Kaenbyou_Rin"},
-		{Color: 0x42d4f4, FriendlyName: "Sunny", SearchString: "Sunny_Milk"},
-		{Color: 0x4b548, FriendlyName: "Daiyousei", SearchString: "Daiyousei"},
-		{Color: 0x42d4f4, FriendlyName: "Maribel", SearchString: "Maribel_Hearn"},
-		{Color: 0x5b0082, FriendlyName: "Byakuren", SearchString: "Hijiri_Byakuren"},
-		{Color: 0x5b9c66, FriendlyName: "Eiki", SearchString: "Shiki_Eiki"},
-		{Color: 0xe5ff, FriendlyName: "Vert", SearchString: "Cirno"},
-		{Color: 0xfb959e, FriendlyName: "Kasen", SearchString: "Ibaraki_Kasen"},
-		{Color: 0xff40d9, FriendlyName: "Yuyuko", SearchString: "Saigyouji_Yuyuko"},
-		{Color: 0x42d4f4, FriendlyName: "Clownpiece", SearchString: "Clownpiece"},
-		{Color: 0x42d4f4, FriendlyName: "Doremy", SearchString: "Doremy_Sweet"},
-		{Color: 0x42d4f4, FriendlyName: "Yuugi", SearchString: "Hoshiguma_Yuugi"},
-		{Color: 0x42d4f4, FriendlyName: "Yukari", SearchString: "Yakumo_Yukari"},
-		{Color: 0x42d4f4, FriendlyName: "Youka", SearchString: "Kazami_Youka"},
-		{Color: 0xe69454, FriendlyName: "Okina", SearchString: "Matara_Okina"},
-		{Color: 0x62f500, FriendlyName: "Koishi", SearchString: "Komeiji_Koishi"},
-		{Color: 0x42d4f4, FriendlyName: "Suwako", SearchString: "Moriya_Suwako"},
-		{Color: 0x42d4f4, FriendlyName: "Shizuha", SearchString: "Aki_Shizuha"},
-		{Color: 0x42d4f4, FriendlyName: "Ichirin", SearchString: "Kumoi_Ichirin"},
-		{Color: 0xf5da42, FriendlyName: "Rumia", SearchString: "Rumia"},
-		{Color: 0x42d4f4, FriendlyName: "Narumi", SearchString: "Yatadera_Narumi"},
-		{Color: 0x42d4f4, FriendlyName: "Kosuzu", SearchString: "Motoori_Kosuzu"},
-		{Color: 0x6b87bd, FriendlyName: "Seiran", SearchString: "Seiran_(touhou)"},
-		{Color: 0xfbd55a, FriendlyName: "Junko", SearchString: "Junko_(touhou)"},
-		{Color: 0x42d4f4, FriendlyName: "Murasa", SearchString: "Murasa_Minamitsu"},
-		{Color: 0x42d4f4, FriendlyName: "Akyuu", SearchString: "Hieda_No_Akyuu"},
-		{Color: 0x48cb5, FriendlyName: "Shion", SearchString: "Yorigami_Shion"},
-		{Color: 0x42d4f4, FriendlyName: "Elly", SearchString: "Elly"},
-	}
-}
 
 func init() {
 	multiplexer.NotTargeted = append(multiplexer.NotTargeted, guessResponse)
@@ -223,52 +31,16 @@ func init() {
 	})
 }
 
-func fetch(character CharacterInfo) (CharacterArt, error) {
-	var (
-		response *http.Response
-		data     []byte
-		result   BoardPosts
-		post     BoardPost
-	)
-	response, err = http.Get(ImageBoardQueryURL + character.SearchString)
-	if err != nil {
-		return CharacterArt{}, err
-	}
-	defer func() { _ = response.Body.Close() }()
-	data, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		return CharacterArt{}, err
-	}
-	err = xml.Unmarshal(data, &result)
-	if err != nil {
-		return CharacterArt{}, err
-	}
-	rand.Seed(time.Now().UnixNano())
-	if result.Count < 1 {
-		return CharacterArt{}, ErrNoArtAvailable
-	}
-	if result.Count < 100 {
-		post = result.Posts[rand.Intn(result.Count-1)]
-	} else {
-		post = result.Posts[rand.Intn(99)]
-	}
-	return CharacterArt{
-		ImageURL:  post.FileURL,
-		SourceURL: post.Source,
-		Character: character,
-	}, nil
-}
-
 func touhou(context *multiplexer.Context) {
 	var name string
-	var char CharacterInfo
+	var char imagefetch.CharacterInfo
 	var text string
 
 	if len(context.Fields) > 1 {
 		name = strings.ToLower(context.Fields[1])
 	}
 
-	for _, character := range CharacterList() {
+	for _, character := range imagefetch.Touhou.Characters() {
 		if name == strings.ToLower(character.FriendlyName) {
 			char = character
 		}
@@ -276,12 +48,12 @@ func touhou(context *multiplexer.Context) {
 
 	if char.SearchString == "" {
 		text = "No character matched your query, displaying random character."
-		char = CharacterList()[rand.Intn(len(CharacterList()))]
+		char = imagefetch.Touhou.Characters()[rand.Intn(len(imagefetch.Touhou.Characters()))]
 	}
 
-	var art CharacterArt
-	art, err = fetch(char)
-	if err == ErrNoArtAvailable {
+	var art imagefetch.CharacterArt
+	art, err = imagefetch.Fetch(char)
+	if err == imagefetch.ErrNoArtAvailable {
 		context.SendMessage("No art available for this character.")
 		return
 	}
@@ -320,10 +92,10 @@ func guess(context *multiplexer.Context) {
 	sessions[context.Message.ChannelID] = message
 	defer func() { delete(sessions, context.Message.ChannelID) }()
 
-	var art CharacterArt
-	char := CharacterList()[rand.Intn(len(CharacterList()))]
-	art, err = fetch(char)
-	if err == ErrNoArtAvailable {
+	var art imagefetch.CharacterArt
+	char := imagefetch.Touhou.Characters()[rand.Intn(len(imagefetch.Touhou.Characters()))]
+	art, err = imagefetch.Fetch(char)
+	if err == imagefetch.ErrNoArtAvailable {
 		context.SendMessage("No art available for this character.")
 		return
 	}
