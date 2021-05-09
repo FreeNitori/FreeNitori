4ea74d0521f2b6133073dc578be62a8521e6fd0e
RandomChars <random@chars.jp>
2021-05-09 15:37:34 +0900

    remake homepage

--- cmd/server/web/assets/public/js/index.js
+++ cmd/server/web/assets/public/js/index.js
@@ -5,6 +5,7 @@ let stats = fetchJSON("/api/stats")
 info.then(function (data) {
     document.getElementById("inviteURL").href = data["invite_url"];
     document.getElementById("programVersion").textContent = data["nitori_version"];
+    document.getElementById("programRevision").textContent = data["nitori_revision"];
 })
 stats.then(function (data) {
     document.getElementById("messageTotal").textContent = data["total_messages"];
