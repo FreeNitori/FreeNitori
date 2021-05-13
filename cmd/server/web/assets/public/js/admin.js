eab80287175beac4ced367e6d96c0393cd6da088
RandomChars <random@chars.jp>
2021-05-14 00:38:19 +0900

    optimise styling so cover takes up entire view, display stats in administration page,

--- cmd/server/web/assets/public/js/admin.js
+++ cmd/server/web/assets/public/js/admin.js
@@ -7,6 +7,27 @@ fetchJSON("/api/nitori").then(function (data) {
     nitoriData = data;
 });
 
+// Populate from /api/nitori/stats
+fetchJSON("/api/nitori/stats").then(function (data) {
+    // Process
+    document.getElementById("PID").innerText = data["Process"]["PID"];
+    document.getElementById("Uptime").innerText = data["Process"]["Uptime"];
+    document.getElementById("NumGoroutine").innerText = data["Process"]["NumGoroutine"];
+    document.getElementById("DBSize").innerText = data["Process"]["DBSize"];
+
+    // Platform
+    document.getElementById("GoVersion").innerText = data["Platform"]["GoVersion"];
+    document.getElementById("GOOS").innerText = data["Platform"]["GOOS"];
+    document.getElementById("GOARCH").innerText = data["Platform"]["GOARCH"];
+    document.getElementById("GOROOT").innerText = data["Platform"]["GOROOT"];
+
+    // Discord
+    document.getElementById("Intents").innerText = data["Discord"]["Intents"];
+    document.getElementById("Sharding").innerText = data["Discord"]["Sharding"];
+    document.getElementById("Shards").innerText = data["Discord"]["Shards"];
+    document.getElementById("Guilds").innerText = data["Discord"]["Guilds"];
+});
+
 function changeUsername() {
     nitoriData["Name"] = document.getElementById("newUsername").value;
     postJSON("/api/nitori", nitoriData).then(function (data) {
