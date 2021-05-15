81bafffcfafbf3a4d9154f7435d749aeb0f9bbf4
RandomChars <random@chars.jp>
2021-05-15 23:30:56 +0900

    self healing web socket

--- cmd/server/web/assets/public/js/logs.js
+++ cmd/server/web/assets/public/js/logs.js
@@ -1,13 +1,34 @@
 let logs = document.getElementById("logs");
 let ws;
-if (window.location.protocol === "https:") {
-    ws = new WebSocket("wss://" + window.location.host + "/api/nitori/logs");
-} else {
-    ws = new WebSocket("ws://" + window.location.host + "/api/nitori/logs");
-}
-ws.onmessage = function (entry) {
-    logs.value += entry.data;
-    if (!entry.data.endsWith("\n")) {
-        logs.value += "\n";
+openSocket();
+
+function openSocket() {
+    if (window.location.protocol === "https:") {
+        ws = new WebSocket("wss://" + window.location.host + "/api/nitori/logs");
+    } else {
+        ws = new WebSocket("ws://" + window.location.host + "/api/nitori/logs");
     }
-};
+
+    ws.onopen = function (_) {
+        console.log("Socked opened.");
+    };
+
+    ws.onmessage = function (event) {
+        logs.value += event.data;
+        if (!event.data.endsWith("\n")) {
+            logs.value += "\n";
+        }
+    };
+
+    ws.onclose = function (_) {
+        console.log("Socket close.");
+        setTimeout(function () {
+            openSocket();
+        }, 1000);
+    };
+
+    ws.onerror = function (_) {
+        console.error("Socket error.");
+        ws.close();
+    }
+}
