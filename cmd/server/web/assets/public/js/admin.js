74b6c610923c0bdef00d10cf6d4158e90e6201d1
RandomChars <random@chars.jp>
2021-05-15 23:14:22 +0900

    get rid of charts, complete the displaying of all stats

--- cmd/server/web/assets/public/js/admin.js
+++ cmd/server/web/assets/public/js/admin.js
@@ -26,6 +26,40 @@ fetchJSON("/api/nitori/stats").then(function (data) {
     document.getElementById("Sharding").innerText = data["Discord"]["Sharding"];
     document.getElementById("Shards").innerText = data["Discord"]["Shards"];
     document.getElementById("Guilds").innerText = data["Discord"]["Guilds"];
+
+    // Mem
+    document.getElementById("MemAllocated").innerText = data["Mem"]["Allocated"];
+    document.getElementById("MemTotal").innerText = data["Mem"]["Total"];
+    document.getElementById("MemSys").innerText = data["Mem"]["Sys"];
+    document.getElementById("MemLookups").innerText = data["Mem"]["Lookups"];
+    document.getElementById("MemMallocs").innerText = data["Mem"]["Mallocs"];
+    document.getElementById("MemFrees").innerText = data["Mem"]["Frees"];
+
+    // Heap
+    document.getElementById("HeapAlloc").innerText = data["Heap"]["Alloc"];
+    document.getElementById("HeapSys").innerText = data["Heap"]["Sys"];
+    document.getElementById("HeapIdle").innerText = data["Heap"]["Idle"];
+    document.getElementById("HeapInuse").innerText = data["Heap"]["Inuse"];
+    document.getElementById("HeapReleased").innerText = data["Heap"]["Released"];
+    document.getElementById("HeapObjects").innerText = data["Heap"]["Objects"];
+
+    // GC
+    document.getElementById("NextGC").innerText = data["GC"]["NextGC"];
+    document.getElementById("LastGC").innerText = data["GC"]["LastGC"];
+    document.getElementById("PauseTotalNs").innerText = data["GC"]["PauseTotalNs"];
+    document.getElementById("PauseNs").innerText = data["GC"]["PauseNs"];
+    document.getElementById("NumGC").innerText = data["GC"]["NumGC"];
+
+    // Misc
+    document.getElementById("StackInuse").innerText = data["Misc"]["StackInuse"];
+    document.getElementById("StackSys").innerText = data["Misc"]["StackSys"];
+    document.getElementById("MSpanInuse").innerText = data["Misc"]["MSpanInuse"];
+    document.getElementById("MSpanSys").innerText = data["Misc"]["MSpanSys"];
+    document.getElementById("MCacheInuse").innerText = data["Misc"]["MCacheInuse"];
+    document.getElementById("MCacheSys").innerText = data["Misc"]["MCacheSys"];
+    document.getElementById("GCSys").innerText = data["Misc"]["GCSys"];
+    document.getElementById("BuckHashSys").innerText = data["Misc"]["BuckHashSys"];
+    document.getElementById("OtherSys").innerText = data["Misc"]["OtherSys"];
 });
 
 function changeUsername() {
